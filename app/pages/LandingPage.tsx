9793769607d40de963eb999a28a89c15b55259eb
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-12-16 10:53:13 +0545

    change_design: Change in the design with minimal look

--- app/pages/LandingPage.tsx
+++ app/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Lilita_One, Play } from 'next/font/google'
 import { Parkinsans } from 'next/font/google';
 import { Button } from '@/components/ui/button';
-import { ChevronRight, PlayIcon } from 'lucide-react';
+import { ChevronRight, LaptopIcon, PlayIcon } from 'lucide-react';
 import { Oswald } from 'next/font/google';
 import { Raleway } from 'next/font/google';
 import { Catamaran } from 'next/font/google';
@@ -54,30 +54,10 @@ const parkinsans = Parkinsans({
 
 const LandingPage = () => {
   return (
-    <div className='flex flex-col items-center min-h-screen w-full'>
-    <div className='flex flex-col w-[60vw] items-center mt-[10%]'>
-        <h1 className={`${roboto_slab.className} text-[60px] text-center leading-tight`}>DevShowcase: <span className='text-[#96938f] leading-tight'>Your Digital Development Portfolio</span></h1>
-
-        <p className='w-[80%] text-center mt-[1.5rem] font-semibold'>A revolutionary platform designed to help developers showcase their projects, highlight their skills, and connect with the global tech community.</p>
-
-        <div className='flex items-center justify-between gap-5'>
-            <Button size={'lg'} className='text-[16px] font-medium mt-[1.5rem]'>
-                Get Started 
-                <ChevronRight className='text-white' />
-            </Button>
-
-            <Button size={'lg'} className='text-[16px] font-medium mt-[1.5rem] bg-white text-black hover:bg-white'>
-                Watch Demo
-                <PlayIcon className='text-black'/>
-            </Button>
-        </div>
-    </div>
-
-    <Button className="w-[60vw] h-[18vh] rounded-xl mt-[4rem] bg-white hover:bg-white shadow-lg">
-        
-    </Button>
+    <div className='flex flex-col items-center min-h-screen w-full bg-[#151619]'>
+   
     
-</div>
+    </div>
   )
 }
 
