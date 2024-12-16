db1eaed93118b8029fda29dfc60523ba569e25a5
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-12-16 11:32:53 +0545

    logo_change: Changed the logo for the website

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 left-0 h-20 w-full flex items-center px-4'>
-        <Image src = '/logo.png' alt='logo' height={20} width={190}></Image>
+    <div className='fixed top-0 left-0 h-20 w-full flex items-center px-6'>
+        <Image src = '/logo.png' alt='logo' height={20} width={155}></Image>
     </div>
   )
 }
