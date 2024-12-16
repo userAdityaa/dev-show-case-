1ce6276d9c46257afa8c54030dfed710bae43ea0
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-12-16 11:12:49 +0545

    landing_page: Changed the landing page to more minimal design

--- app/pages/LandingPage.tsx
+++ app/pages/LandingPage.tsx
@@ -9,6 +9,13 @@ import { Catamaran } from 'next/font/google';
 import { Signika } from 'next/font/google';
 import { Oregano } from 'next/font/google';
 import { Roboto_Slab } from 'next/font/google';
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+
+const poppins = Poppins({
+    subsets: ['latin'], 
+    weight: '600', 
+}); 
 
 const lilita_one = Lilita_One({
     subsets: ['latin'],
@@ -54,8 +61,17 @@ const parkinsans = Parkinsans({
 
 const LandingPage = () => {
   return (
-    <div className='flex flex-col items-center min-h-screen w-full bg-[#151619]'>
-   
+    <div className='flex justify-between items-center min-h-screen w-full bg-[#151619] p-6'>
+        <div className={`flex flex-col w-[50%] h-[20rem] gap-2`}>
+            <h1 className={`text-[#fbf8f0] text-[52px] font-bold ${poppins.className}`}>Your Digital Development Portfolio</h1>
+            <p className='text-[#fbf8f0] font-medium text-[19px]'>A revolutionary platform designed to help developers showcase their projects, highlight their skills, and connect with the global tech community.</p>
+            <div className='flex justify-between w-[50%] text-[20px] mt-4'>
+                <Button className='bg-white text-black font-semibold p-6 rounded-xl hover:text-white hover:border hover:border-zinc-300'>Get Started Now</Button>
+                <Button className='text-white border border-zinc-400 p-6 rounded-xl hover:bg-white hover:text-black'>Watch Demo <PlayIcon></PlayIcon></Button>
+            </div>
+        </div>
+
+        <Image src='/cloud_landing.webp' alt='cloud landing page' height={300} width={800}></Image>
     
     </div>
   )
