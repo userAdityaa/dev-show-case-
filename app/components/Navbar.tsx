d985934057ec15485f537a99133ca99b9a8955f3
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-17 21:33:05 +0545

    client_side: Included client side rendering

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
+'use client'
+import { useNavbarTheme } from '../context/navbarContext';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 const Navbar = () => {
-  return (
-    <div className='fixed top-0 left-0 h-20 w-full flex items-center px-6 justify-between'>
-        <Image src = '/logo.png' alt='logo' height={20} width={155}></Image>
-        <Button className='bg-white text-black font-semibold hover:border hover:border-zinc-300 hover:text-white'>Sign in</Button>
-    </div>
-  )
-}
+    const { theme } = useNavbarTheme();
 
-export default Navbar
+    return (
+        <div className='fixed top-0 left-0 h-20 w-full flex items-center px-6 justify-between'>
+            <Image src='/logo.png' alt='logo' height={20} width={155} />
+            {theme === 'LandingPage' && (
+                <Button className='bg-white text-black font-semibold hover:border hover:border-zinc-300 hover:text-white'>
+                    Sign in
+                </Button>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
