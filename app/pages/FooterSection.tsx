3fc02f8d32c4028e295fc4bc7ed2c21a3e108708
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-17 22:39:50 +0545

    responsive for footer section

--- app/pages/FooterSection.tsx
+++ app/pages/FooterSection.tsx
@@ -10,11 +10,11 @@ const poppins = Poppins({
 const FooterSection = () => {
     return (
       <div className='bg-zinc-800 w-full h-[30rem] mt-[6rem] flex flex-col  items-center relative'>
-          <h1 className={`text-white text-opacity-100 opacity-100 font-bold z-40 ${poppins.className} text-[30px] w-96 text-center mt-14`}>Ready to Showcase Your Development Journey?</h1>
-          <p className='text-white text-[20px] w-[60%] text-center mt-6'>Join LaunchDesk and transform how you present your projects to the world. No more complex deployments or limited portfolios.</p>
-          <div className='flex gap-4 mt-8'>
-            <Button className='p-6 text-[18px] rounded-xl bg-white text-black hover:text-white'>Create Free Account</Button>
-            <Button className='p-6 text-[18px] rounded-xl hover:border hover:border-zinc-400'>Learn More</Button>
+          <h1 className={`text-white text-opacity-100 opacity-100 font-bold z-40 ${poppins.className} text-[30px] w-96 text-center mt-14 max-md:text-[25px]`}>Ready to Showcase Your Development Journey?</h1>
+          <p className='text-white text-[20px] w-[60%] text-center mt-6 max-md:w-[80%] max-md:text-[19px] max-md:text-opacity-80'>Join LaunchDesk and transform how you present your projects to the world. No more complex deployments or limited portfolios.</p>
+          <div className='flex gap-4 mt-8 max-md:flex-col'>
+            <Button className='p-6 text-[18px] rounded-xl bg-white text-black hover:text-white max-md:p-2 max-md:text-[15px] max-md:font-semibold'>Create Free Account</Button>
+            <Button className='p-6 text-[18px] rounded-xl hover:border hover:border-zinc-400 max-md:p-2 max-md:text-[15px] max-md:font-semibold'>Learn More</Button>
           </div>
       </div>
     )
